a97be3b9da396ce37628896e6fcd920b1ba728d3
chunkind <chunkind@naver.com>
2025-09-29 20:50:15 +0900

    9/29: - TimeManager 생성

--- client/CTimeMgr.h
+++ client/CTimeMgr.h
@@ -6,7 +6,16 @@ class CTimeMgr
 
 private:
 	LARGE_INTEGER	m_llCurCount;
+	//new
+	LARGE_INTEGER	m_llPrevCount;
+
 	LARGE_INTEGER	m_llFrequency;
+
+//new
+	double			m_dDT; // 프레임 간의 시간값
+	double			m_dAcc; // 1초 체크를 위한 누적 시간
+	UINT			m_iCallCount; // 함수 호출 횟수 체크
+	UINT			m_iFPS; // 초당 호출 횟수
 
 public:
 	void init();
