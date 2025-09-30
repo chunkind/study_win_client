9525a245f130e6cf548dd78832bc4b567bbdf480
chunkind <chunkind@naver.com>
2025-09-30 22:13:17 +0900

    9/30: - Monster 생성

--- client/CScene_Start.cpp
+++ client/CScene_Start.cpp
@@ -1,7 +1,8 @@
-#include "Pch.h"
+#include "pch.h"
 #include "CScene_Start.h"
 #include "CObject.h"
 #include "CPlayer.h"
+#include "CMonster.h"
 
 CScene_Start::CScene_Start()
 {
@@ -18,8 +19,14 @@ void CScene_Start::Enter()
 	pObj->SetScale(Vec2(100.f, 100.f));
 	AddObject(pObj, GROUP_TYPE::DEFAULT);
 
+	CMonster* pMonsterObj = new CMonster;
+	pMonsterObj->SetPos(Vec2(640.f, 50.f));
+	pMonsterObj->SetCenterPos(pMonsterObj->GetPos());
+	pMonsterObj->SetScale(Vec2(50.f, 50.f));
+	AddObject(pMonsterObj, GROUP_TYPE::DEFAULT);
 }
 
 void CScene_Start::Exit()
 {
+
 }
