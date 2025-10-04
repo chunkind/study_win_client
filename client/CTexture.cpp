ab26a2532e5676bc55adf2f7447b9117347e0074
chunkind <chunkind@naver.com>
2025-10-04 17:53:02 +0900

    10/4: - 이미지 로드

--- client/CTexture.cpp
+++ client/CTexture.cpp
@@ -2,9 +2,21 @@
 #include "CTexture.h"
 
 CTexture::CTexture()
+	: m_dc(0)
+	, m_hBit(0)
 {
 }
 
 CTexture::~CTexture()
 {
+}
+
+void CTexture::Load(const wstring& _strFilePath)
+{
+	m_hBit = (HBITMAP)LoadImage(nullptr
+		, _strFilePath.c_str()
+		, IMAGE_BITMAP
+		, 0, 0
+		, LR_CREATEDIBSECTION | LR_LOADFROMFILE
+	);
 }
--- client/CTexture.h
+++ client/CTexture.h
@@ -4,6 +4,13 @@
 
 class CTexture : public CRes
 {
+private:
+	HDC			m_dc;
+	HBITMAP		m_hBit;
+
+public:
+	void Load(const wstring& _strFilePath);
+
 public:
 	CTexture();
 	~CTexture();
