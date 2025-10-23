df83ca55efc133ee43c280ff52f2f7a030ce1d8e
1328192 <chunkind@naver.com>
2025-10-23 12:23:01 +0900

    10/23: 유니티 빌드

--- client/pch.h
+++ client/pch.h
@@ -1,25 +1,3 @@
 #pragma once
 
-#include <Windows.h>
-
-#include <iostream>
-
-#include <vector>
-using std::vector;
-
-#include <map>
-using std::map;
-using std::make_pair;
-
-#include <string>
-using std::string;
-using std::wstring;
-
-#include <math.h>
-#include <assert.h>
-
-#pragma comment(lib, "Msimg32.lib")
-
-#include "Define.h"
-#include "Struct.h"
-#include "Func.h"
+#include "Global.h"
